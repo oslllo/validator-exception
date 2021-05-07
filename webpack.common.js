b8a0f0f4acacf745bd99e184cd0e0a90475ddedb
Ghustavh Ehm <ghustavh97@gmail.com>
2021-05-07 16:43:33 +0200

    feat!: drop support for pre-packaged dist bundles and update to validator v2

--- webpack.common.js
+++ webpack.common.js
@@ -17,6 +17,6 @@ module.exports = {
   output: {
     library: "assert",
     libraryTarget: "umd",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
   },
 };
